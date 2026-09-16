bab56d7abdd8bdc782a21b2fa547f4b00a966d0b
sandi <garcisandi310@gmail.com>
2026-09-16 00:38:14 -0500

    Implementa orden de resultados

--- Frontend/assets/js/peliculas.js
+++ Frontend/assets/js/peliculas.js
@@ -48,3 +48,17 @@ document.getElementById('filtro-anio').addEventListener('change', function() {
     : peliculas;
   renderPeliculas(filtradas);
 });
+document.getElementById('filtro-orden').addEventListener('change', function() {
+  const orden = this.value;
+  let ordenadas = [...peliculas];
+
+  if (orden === "valorados") {
+    ordenadas.sort((a, b) => b.rating - a.rating);
+  } else if (orden === "alfabetico") {
+    ordenadas.sort((a, b) => a.titulo.localeCompare(b.titulo));
+  } else {
+    ordenadas.sort((a, b) => b.anio - a.anio);
+  }
+
+  renderPeliculas(ordenadas);
+});
